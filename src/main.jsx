7768d920417adaaf1d85aa23c859fdaf6de1c1be
Tejasva Jaitly <tejasvajaitly@gmail.com>
2023-04-01 16:09:32 -0700

    feat: get, create, update, delete, error boundary for contacts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import ErrorPage from "./error-page";
+import Index from "./routes/index";
+import Contact, {loader as contactLoader} from "./routes/contact";
+import EditContact, {action as editAction} from "./routes/edit";
+import {action as destroyAction} from "./routes/destroy";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
